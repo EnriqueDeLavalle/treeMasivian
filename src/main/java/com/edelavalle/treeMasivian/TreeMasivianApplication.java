0c633a6c93a934aece056d121defa33a50fa21b2
Enrique De Lavalle <enriquedelavalle@gmail.com>
2019-10-11 15:15:03 -0500

    Base commit

---
+++ src/main/java/com/edelavalle/treeMasivian/TreeMasivianApplication.java
@@ -0,0 +1,13 @@
+package com.edelavalle.treeMasivian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeMasivianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeMasivianApplication.class, args);
+	}
+
+}
